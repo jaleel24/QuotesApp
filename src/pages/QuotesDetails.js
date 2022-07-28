8d6b69dfc3a26b00cb6c6397ec9f91eff1b414e0
jaleel24 <36284471+jaleel24@users.noreply.github.com>
2022-07-28 13:18:21 -0400

    added a functionality to show quotesDetails and made the show detail button work,plus it helps us navigate to the details of the specific quote with specific id

--- src/pages/QuotesDetails.js
+++ src/pages/QuotesDetails.js
@@ -1,12 +1,33 @@
 import { Fragment } from "react";
-import { useParams ,Route} from "react-router-dom";
+import { useParams, Route } from "react-router-dom";
 import Comments from "../components/comments/Comments";
-const QuotesDetails = () => {
+import HighLightedQuote from '../components/quotes/HighlightedQuote';
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: "Jaleel",
+    text: "Never put your guards down so quickly",
+  },
+  {
+    id: "q2",
+    author: "Sana",
+    text: "Love for all hatred for none",
+  },
+];
+const QuotesDetails = (props) => {
   const param = useParams();
+  const quote = DUMMY_QUOTES.find( quotes=> quotes.id === param.quotesId)
+
+  if(!quote){
+    return <p>No Quote Found !</p>
+  }
   return (
     <Fragment>
       <h1> Quotes Details page with id : {param.quotesId}</h1>
-      <Route path={`/quotes/${param.quotesId}/comments`} ><Comments /></Route>
+      <HighLightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${param.quotesId}/comments`}>
+        <Comments />
+      </Route>
     </Fragment>
   );
 };
